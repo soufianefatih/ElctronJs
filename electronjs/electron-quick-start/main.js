38b3bc921cd6c7746cd9e71f4658561ad416add2
salahhusa9 <salahhusa9@gmail.com>
2022-06-06 09:21:45 +0100

    Clear && Analyser

--- electronjs/electron-quick-start/main.js
+++ electronjs/electron-quick-start/main.js
@@ -1,19 +1,22 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
--- electronjs/electron-quick-start/renderer.js
+++ electronjs/electron-quick-start/renderer.js
@@ -1,6 +1,65 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const fs = require('fs');
+const clearButton = document.getElementById('clearButton')
+
+if (clearButton) {
+    clearButton.addEventListener("click", function () {
+        console.log('clearButton')
+
+        startRemoveing('C:\\Users\\Youcode\\AppData\\Local\\Temp');
+        dirSize('C:\\Users\\Youcode\\AppData\\Local\\Temp')
+    })
+}
+
+function dirSize(path) {
+    var dirSizePath = path;
+    fs.readdir(path, function (err, files) {
+        let size = 0
+        files.forEach(function (file) {
+            size += fileSize(dirSizePath + '\\' + file);
+        });
+
+        document.getElementById('analitic_size').innerText = (size / 1024 / 1024) + 'Mo';
+        console.log('size: ', size / 1024 / 1024, 'Mo')
+    })
+}
+
+function fileSize(file) {
+    if (fs.existsSync(file)) {
+        return fs.statSync(file).size
+    } else {
+        console.error("This file doesn't exist : ", file);
+        return 0;
+    }
+}
+
+function startRemoveing(filePath) {
+    console.warn('Start Removeing in ' + filePath);
+    var filePath = filePath
+    fs.readdir(filePath, function (err, files) {
+
+        if (err) {
+            return console.error('Unable to scan directory: ' + err);
+        }
+
+        files.forEach(function (file) {
+            removeFile(filePath + '\\' + file);
+        });
+
+        console.warn("File Removed successfully : " + files.length + ' in ' + filePath);
+    });
+}
+
+function removeFile(filePath) {
+    if (fs.existsSync(filePath)) {
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                startRemoveing(filePath);
+                // console.error(err);
+            } else {
+                console.log("File succesfully deleted");
+            }
+        });
+    } else {
+        console.error("This file doesn't exist, cannot delete : ", filePath);
+    }
+}
